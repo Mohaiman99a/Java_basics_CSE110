855a1509b7785eed004da5e366ee3d9fe2c479b0
Mohaiman99a <mohaiman99a@gmail.com>
2026-03-09 15:19:22 +0600

    Create task01.java

---
+++ A4_Linear Loops/task01.java
@@ -0,0 +1,36 @@
+//1a
+public class Task1{
+  public static void main(String[] args){
+
+    int start=24;
+     while(start>=-6){
+      if(start>-6){
+        System.out.print(start + ", ");
+      }
+      else if (start==-6){
+        System.out.println(start);
+      }
+      start-=6;
+    }
+  }
+}
+
+
+
+
+
+//1b
+public class Task2{
+  public static void main(String[] args){
+      int start=-10;
+      while(start<=20){
+      if(start<20){
+      System.out.print(start + ", ");
+      }
+      else if (start==20){
+      System.out.println(start);
+      }
+      start+=5;
+    } 
+  }
+}
